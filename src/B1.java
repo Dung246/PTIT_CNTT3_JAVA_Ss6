e9ff90f169e67fe5b2cb18f91a28aa09664db6cb
Dung246 <dungoo2605@gmail.com>
2026-02-01 23:51:48 +0700

    Add Student class and display student info

--- src/B1.java
+++ src/B1.java
@@ -1,2 +1,32 @@
+class Student {
+    String studentId;
+    String fullName;
+    int birthYear;
+    double gpa;
+
+    Student(String studentId, String fullName, int birthYear, double gpa) {
+        this.studentId = studentId;
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+        this.gpa = gpa;
+    }
+
+    void displayInfo() {
+        System.out.println("----------------------");
+        System.out.println("Mã sinh viên: " + studentId);
+        System.out.println("Họ tên: " + fullName);
+        System.out.println("Năm sinh: " + birthYear);
+        System.out.println("Điểm trung bình: " + gpa);
+        System.out.println("----------------------");
+    }
+}
+
 public class B1 {
+    public static void main(String[] args) {
+        Student student1 = new Student("PTIT001", "Nguyễn Trần Bảo Khánh", 2006, 8.5);
+        Student student2 = new Student("PTIT002", "Bàng Đệ Tam", 2005, 8.8);
+
+        student1.displayInfo();
+        student2.displayInfo();
+    }
 }
