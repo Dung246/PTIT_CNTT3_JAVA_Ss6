210f8aac599e64677efb04160cf942209fcd28a7
Dung246 <dungoo2605@gmail.com>
2026-02-01 23:53:27 +0700

    Add Book class and display functionality

--- src/B5.java
+++ src/B5.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+class Book {
+    String bookId;
+    String title;
+    String author;
+    double price;
+
+    Book(String bookId, String title, String author, double price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayInfo() {
+        System.out.println("----------------------");
+        System.out.println("Mã sách: " + bookId);
+        System.out.println("Tên sách: " + title);
+        System.out.println("Tác giả: " + author);
+        System.out.println("Giá bán: " + price);
+        System.out.println("----------------------");
+    }
+}
 
 public class B5 {
+    public static void main(String[] args) {
+        Book firstBook = new Book("IT01", "Harry Potter", "Voldemort", 120000);
+        Book secondBook = new Book("IT02", "Doraemon", "Aoyama", 150000);
+
+        firstBook.displayInfo();
+        secondBook.displayInfo();
+    }
 }
